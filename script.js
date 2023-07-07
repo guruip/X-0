68904b4d5c99c4fa584a1a322dc4338ead9e4dda
guruip <82904567+guruip@users.noreply.github.com>
2023-07-07 22:46:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,79 +1,79 @@
-let area = document.getElementById('area');
-let cell = document.getElementsByClassName('cell');
-let currentPlayer = document.getElementById('cupPlyr');
+const area = document.getElementById('area');
+const cell = document.getElementsByClassName('cell');
+const currentPlayer = document.getElementById('curPlyr');
 
-let player = "x";
-let stat = {
+const player = 'x';
+const stat = {
     'x': 0,
     'o': 0,
     'd': 0
 }
-let winIndex = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7]
+const winIndex = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
 ];
 
-for(let i = 1; i <= 9; i++) {
+for (let i = 1; i <= 9 ; i++) {
     area.innerHTML += "<div class='cell' pos=" + i + "></div>";
 }
-
-for(let i = 0; i < cell.length; i++) {
+for (let i = 0; i < cell.length; i++) {
     cell[i].addEventListener('click', cellClick, false);
 }
 
 function cellClick() {
-
     let data = [];
 
-    if(!this.innerHTML) {
+    if (!this.innerHTML) {
         this.innerHTML = player;
     }else {
-        alert("ячейка занята");
+        alert('Ячейка занята');
         return;
     }
 
-    for(let i in cell) {
-        if(cell[i].innerHTML == player) {
+    for (let i in cell) {
+        if (cell[i].innerHTML == player) {
             data.push(parseInt(cell[i].getAttribute('pos')));
         }
     }
 
-    if(checkWin(data)) {
+    if (checkWin(data)) {
         stat[player] += 1;
-        restart("Выйграл: " + player);
+        restart('Выйграл: ' + player);
     }else {
         let draw = true;
-        for(let i in cell) {
-            if(cell[i].innerHTML == '') draw = false;
+
+        for (let i in cell) {
+            if (cell[i].innerHTML == '') draw = false;
         }
-        if(draw) {
+
+        if (draw) {
             stat.d += 1;
-            restart("Ничья");
+            restart('Ничья');
         }
     }
 
-    player = player == "x" ? "o" : "x";
+    player = player == 'x' ? 'o' : 'x';
     currentPlayer.innerHTML = player.toUpperCase();
 }
 
 function checkWin(data) {
-    for(let i in winIndex) {
+    for (let i in winIndex) {
         let win = true;
-        for(let j in winIndex[i]) {
+
+        for (let j in winIndex[i]) {
             let id = winIndex[i][j];
             let ind = data.indexOf(id);
 
-            if(ind == -1) {
-                win = false
+            if (ind == -1) {
+                win = false;
             }
         }
-        
         if(win) return true;
     }
     return false;
@@ -81,7 +81,8 @@ function checkWin(data) {
 
 function restart(text) {
     alert(text);
-    for( let i = 0; i < cell.length; i++) {
+
+    for (let i = 0; i < cell.length; i++) {
         cell[i].innerHTML = '';
     }
     updateStat();
